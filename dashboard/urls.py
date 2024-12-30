bc96e71bf59470d9de521bf9a8258c82961bbb2a
PyZenMatt <101363711+PyZenMatt@users.noreply.github.com>
2024-12-30 14:13:45 +0100

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,33 +1,9 @@
-"""
-URL configuration for dashboard project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include  # Importa include per includere il router
-from rest_framework.routers import DefaultRouter
-from core.views import DocumentViewSet
+from django.urls import path, include
 
-# Inizializza il router e registra il ViewSet
-router = DefaultRouter()
-router.register(r'documents', DocumentViewSet, basename='document')
-
-# Aggiungi il router alle URL principali
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('api', include(router.urls)),
-    path('core/', include('core.urls')),
-
+    path('api/', include('core.urls')),  # Include gli endpoint API
+    path('auth/', include('dj_rest_auth.urls')),  # Endpoint per login/logout
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),  # Endpoint per registrazione
 ]
