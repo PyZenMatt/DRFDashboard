ec79ebbbadc673d6665c7d43ca310689839609c3
PyZenMatt <101363711+PyZenMatt@users.noreply.github.com>
2024-12-30 14:08:08 +0100

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Document
+from .models import Document, Profile
 
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
-        fields = ['id', 'name', 'file', 'created_at', 'user']
+        fields = ['id', 'name', 'file', 'created_at']
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id', 'date_of_birth', 'residence', 'bio', 'hair_color', 'photo']
